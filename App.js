240df88277d9a37e7e0752e971c847c0be04e86f
donnymays <dtmays2@gmail.com>
2020-12-16 14:24:05 -0800

    functionality to add items to the cart

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import  { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import productsReducer from './store/reducers/products';
+import cartReducer from './store/reducers/cart';
 import ShopNavigator from './navigation/ShopNavigator';
 
 const rootReducer = combineReducers({
-  products: productsReducer
+  products: productsReducer,
+  cart: cartReducer
 });
 
 const store = createStore(rootReducer);
--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -37,7 +37,7 @@ const ProductItem = (props) => {
               />
               <Button
                 color={Colors.primaryColor}
-                title="Go To Cart"
+                title="Add To Cart"
                 onPress={props.onAddToCart}
               />
             </View>
